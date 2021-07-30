2f71c6fe0715f4ce63127d225ece54fa3acba1be
PiyushDubey31 <piyushnit77@gmail.com>
2021-07-30 17:36:20 +0530

    rhird commit

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const route = require('./route');
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 app.use(express.json());
