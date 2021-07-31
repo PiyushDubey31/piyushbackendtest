5c687c415f7708709ee22361d3a6edeeb4c771d7
PiyushDubey31 <piyushnit77@gmail.com>
2021-07-31 12:40:49 +0530

    change in routing

--- route.js
+++ route.js
@@ -9,8 +9,8 @@ const { productController } = require('./api/controllers/productController');
 const { userController } = require('./api/controllers/userController');
 
 const route = require('express').Router();
-route.post('/user', userController);
-route.post('/product', productController);
+route.post('/post/user', userController);
+route.post('/post/product', productController);
 route.post('/post/order', createOrderController);
 route.get('/get/order', getOrderController);
 route.patch('/patch/order', updateOrderController);
